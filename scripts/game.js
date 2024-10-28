2b7c52b0b9219239e4a6734a059ee6686e7f7b8b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-10-28 23:40:32 +0200

    Create - tilemap and layer to display maze

--- scripts/game.js
+++ scripts/game.js
@@ -29,5 +29,18 @@ function preload() {
     frameHeight: 32,
   });
 }
-function create() {}
+function create() {
+  // create a tilemap where each tile is 32*32 px from map created from csv file
+  const map = this.make.tilemap({
+    key: "map",
+    tileWidth: 32,
+    tileHeight: 32,
+  });
+
+  // add tileset image of 32*32 px tile where tile 0 is transparent, 1 is blank tile, 2 is shadowed tile
+  const tileset = map.addTilesetImage("tiles", null, 32, 32, 1, 2);
+
+  // create layer that will draw maze from added tileset
+  this.layer = map.createLayer(0, tileset, 0, 0);
+}
 function update() {}
