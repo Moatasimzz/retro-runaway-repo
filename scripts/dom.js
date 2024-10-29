631d63fbf0c972a55d97e8a06936724b41138a9e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-10-29 14:00:26 +0200

    Add - basic leveling through changing grid

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,1 +1,3 @@
 var scoreText = document.getElementById("score");
+var levelOne = document.getElementById("level1");
+var levelTwo = document.getElementById("level2");
--- scripts/game.js
+++ scripts/game.js
@@ -18,19 +18,37 @@ const config = {
   },
 };
 
-const game = new Phaser.Game(config);
+var game;
 
+var grid1 = "./../assets/tilemaps/grid.csv";
+var grid2 = "./../assets/tilemaps/grid02.csv";
+var grid = "";
 var player,
   coins,
   cursors,
   layer,
   score = 0;
 const coinValue = 3; // The value in your CSV that represents a coin
+levelOne.addEventListener("click", function () {
+  if (game) {
+    game.destroy(true, false);
+  }
+  grid = grid1;
+  game = new Phaser.Game(config);
+});
+levelTwo.addEventListener("click", function () {
+  if (game) {
+    game.destroy(true, false);
+  }
+
+  grid = grid2;
+  game = new Phaser.Game(config);
+});
 
 function preload() {
   this.load.image("tiles", "./../assets/images/purple-tile.png");
-  this.load.image("car", "./../assets/images/yellow-car.png");
-  this.load.tilemapCSV("map", "./../assets/tilemaps/grid02.csv");
+  this.load.image("car", "./../assets/images/green-car.png");
+  this.load.tilemapCSV("map", grid);
   this.load.spritesheet("coin", "./../assets/images/coin.png", {
     frameWidth: 32,
     frameHeight: 32,
