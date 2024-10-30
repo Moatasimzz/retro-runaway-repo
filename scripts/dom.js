45b882cacfaa118c60232ad92c02888e28c710a9
BreicheSinal <sinalbreiche@gmail.com>
2024-10-30 02:29:27 +0200

    Completed functionalities of play/help buttons with validation

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,1 +1,6 @@
 var scoreText = document.getElementById("score");
+
+/*MAIN MENU*/
+var playBttn = document.getElementById("play-bttn");
+var helpBttn = document.getElementById("help-bttn");
+var userName = document.getElementById("username");
